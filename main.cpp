1296e24da49ba81683d5ce59dd7adf7d769989b3
Perfect03 <92153941+Perfect03@users.noreply.github.com>
2022-04-07 15:36:59 +0600

    Adding files with solution from book

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "win.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	Win w;
+	w.show();
+	return a.exec();
+}
---
+++ win.h
@@ -0,0 +1,25 @@
+﻿#ifndef WIN_H
+#define WIN_H
+
+class Win : public QWidget // класс окна
+{
+	Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+
+protected:
+	QFrame *frame; // рамка
+	QLabel *inputLabel; // метка ввода
+	QLineEdit *inputEdit; // строчный редактор ввода
+	QLabel *outputLabel; // метка вывода
+	QLineEdit *outputEdit; // строчный редактор вывода
+	QPushButton *nextButton; // кнопка Следующее
+	QPushButton *exitButton; // кнопка Выход
+
+public:
+	Win(QWidget *parent = nullptr); // конструктор
+
+public slots:
+	void begin(); // метод начальной настройки интерфейса
+	void calc(); // метод реализации вычислений
+};
+
+#endif // WIN_H
